cef5f3bbb4d44722884306da00ff64ea2ecf24b9
JuanPablo-2000 <agudelo.14321@gmail.com>
2022-05-20 00:17:26 -0500

    udemy react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //import React, { Fragment } from 'react';
 import React from 'react';
-<<<<<<< HEAD
 import PropTypes from 'prop-types';
 
 export const App = ({saludo ,subtitule}) => {
@@ -9,21 +8,11 @@ export const App = ({saludo ,subtitule}) => {
         <>
             <h1>{saludo}</h1>
             <p>{subtitule}</p>
-=======
-
-export const App = () => {
-    return (
-        //<Fragment>
-        <>
-            <h1> Hola Mundo </h1>
-            <p> Mi primera aplicación</p>
->>>>>>> 56db3c5243208672409ae930aec02226284cd5e8
         </>
         //</Fragment>
     );
 }
 
-<<<<<<< HEAD
 App.propTypes = {
     saludo: PropTypes.string
 }
@@ -31,6 +20,4 @@ App.propTypes = {
 App.defaultProps = {
     subtitule: 'Esto es un subtitulo'
 }
-=======
 
->>>>>>> 56db3c5243208672409ae930aec02226284cd5e8
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.js';
-<<<<<<< HEAD
 import { CounterApp } from './CounterApp.js';
-=======
->>>>>>> 56db3c5243208672409ae930aec02226284cd5e8
 
 import './index.css';
 
@@ -12,10 +9,6 @@ const container = document.getElementById('root');
 
 const root = createRoot(container);
 
-<<<<<<< HEAD
 root.render(<CounterApp value={1234} />);
     //<App saludo='Hola soy, Goku' 
 // subtitule='Dragon Ball 💢💥✨'/>);
-=======
-root.render(<App />);
->>>>>>> 56db3c5243208672409ae930aec02226284cd5e8
