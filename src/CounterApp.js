5123b0829f4863ba6ebeede34054f5bc22ff5e48
JuanPablo-2000 <agudelo.14321@gmail.com>
2022-05-23 10:03:22 -0500

    Sección 4 - Finalizada

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export const CounterApp = ({value}) => {
+export const CounterApp = ({value = 0}) => {
+
+    const [ counter, setCounter ] = useState( 0 );
+
+    // handleAdd
+    const handleAdd = () => {
+        // setCounter(counter+1);
+        setCounter( (c) => c +1 )
+    }
+
+    const handleSubtract = () => setCounter( counter - 1 );
+    const handleReset = () => setCounter( value );
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
+
+            <button onClick={handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubtract }>-1</button>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App.js';
+// import { App } from './App.js';
 import { CounterApp } from './CounterApp.js';
 
 import './index.css';
@@ -9,6 +9,6 @@ const container = document.getElementById('root');
 
 const root = createRoot(container);
 
-root.render(<CounterApp value={1234} />);
+root.render(<CounterApp value={10} />);
     //<App saludo='Hola soy, Goku' 
 // subtitule='Dragon Ball 💢💥✨'/>);
